598339af919a4669b9bac4fa3750e52678fe20c1
timepp <tongjunhui@live.cn>
2024-06-24 11:32:10 +0800

    add test file using jsr

---
+++ testjsr.ts
@@ -0,0 +1,8 @@
+import 'jsr:@timepp/zero-config-deno-log'
+
+console.log('Hello, World!')
+console.error('Errors!')
+console.warn('Warnings!')
+console.info('Information!')
+console.debug('Debugging!')
+console.error('multi line\nerrors!\nanother line!')
